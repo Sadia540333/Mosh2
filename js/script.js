f0c5e67283e25cfc2be6171da5184a760c0669e8
sadia554466 <pue.khulna@gmail.com>
2023-03-19 17:29:11 +0600

    a

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.review').slick({
+  dots: false,
+  arrows:false,
+  infinite: true,
+  speed:300,
+  autoplay: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '0px',
+});
+
+$('.counter').counterUp({
+	delay: 10,
+	time: 1000
+});
